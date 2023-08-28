93caf3f8dfa2413a2623146f076d362ca7fbcbca
Pavel Yosifovich <zodiacon@live.com>
2023-08-27 22:18:00 -0400

    ObjectTypes sample

---
+++ Chapter07/ObjectTypes/ObjectTypes.cpp
@@ -0,0 +1,34 @@
+// ObjectTypes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+
+#pragma comment(lib, "ntdll")
+
+int main() {
+	auto size = 1 << 16;
+	auto buffer = std::make_unique<BYTE[]>(size);
+	if (NT_SUCCESS(NtQueryObject(nullptr, ObjectTypesInformation, buffer.get(), size, nullptr))) {
+		auto types = (OBJECT_TYPES_INFORMATION*)buffer.get();
+		//
+		// first type starts at offset of pointer-size bytes 
+		//
+		auto type = (OBJECT_TYPE_INFORMATION*)((PBYTE)types + sizeof(PVOID));
+		for (ULONG i = 0; i < types->NumberOfTypes; i++) {
+			printf("%-33wZ (%2d) O: %7u H: %7u PO: %7u PH: %7u\n",
+				&type->TypeName, type->TypeIndex,
+				type->TotalNumberOfObjects, type->TotalNumberOfHandles,
+				type->HighWaterNumberOfObjects, type->HighWaterNumberOfHandles);
+
+			// move to next type object
+
+			auto temp = (PBYTE)type + sizeof(OBJECT_TYPE_INFORMATION) + type->TypeName.MaximumLength;
+			//
+			// round up to the next pointer-size address
+			//
+			type = (OBJECT_TYPE_INFORMATION*)(((ULONG_PTR)temp + sizeof(PVOID) - 1) / sizeof(PVOID) * sizeof(PVOID));
+		}
+		printf("Total Types: %u\n", types->NumberOfTypes);
+	}
+	return 0;
+}
