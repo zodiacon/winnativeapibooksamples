58fa87e72614ab2c08842b915111d3786ca3cfd5
Pavel Yosifovich <zodiacon@live.com>
2024-06-10 10:12:00 -0400

    key info and enum key samples

---
+++ Chapter13/EnumKeyValues/EnumKeyValues.cpp
@@ -0,0 +1,113 @@
+#include <phnt_windows.h>
+#include <phnt.h>
+#include <stdio.h>
+#include <memory>
+#include <string>
+
+#pragma comment(lib, "ntdll")
+
+
+PCSTR RegistryTypeToString(DWORD type) {
+	switch (type) {
+		case REG_SZ: return "REG_SZ";
+		case REG_DWORD: return "REG_DWORD";
+		case REG_MULTI_SZ: return "REG_MULTI_SZ";
+		case REG_QWORD: return "REG_QDWORD";
+		case REG_EXPAND_SZ: return "REG_EXPAND_SZ";
+		case REG_NONE: return "REG_NONE";
+		case REG_LINK: return "REG_LINK";
+		case REG_BINARY: return "REG_BINARY";
+		case REG_RESOURCE_REQUIREMENTS_LIST: return "REG_RESOURCE_REQUIREMENTS_LIST";
+		case REG_RESOURCE_LIST: return "REG_RESOURCE_LIST";
+		case REG_FULL_RESOURCE_DESCRIPTOR: return "REG_FULL_RESOURCE_DESCRIPTOR";
+	}
+	return "<unknown>";
+}
+
+void DisplayData(KEY_VALUE_FULL_INFORMATION const* info) {
+	auto p = (PBYTE)info + info->DataOffset;
+	switch (info->Type) {
+		case REG_SZ:
+		case REG_EXPAND_SZ:
+			printf("%ws\n", (PCWSTR)p);
+			break;
+
+		case REG_MULTI_SZ:
+		{
+			auto s = (PCWSTR)p;
+			while (*s) {
+				printf("%ws ", s);
+				s += wcslen(s) + 1;
+			}
+			printf("\n");
+			break;
+		}
+
+		case REG_DWORD:
+			printf("%u (0x%X)\n", *(DWORD*)p, *(DWORD*)p);
+			break;
+
+		case REG_QWORD:
+			printf("%llu (0x%llX)\n", *(ULONGLONG*)p, *(ULONGLONG*)p);
+			break;
+
+		case REG_BINARY:
+		case REG_FULL_RESOURCE_DESCRIPTOR:
+		case REG_RESOURCE_LIST:
+		case REG_RESOURCE_REQUIREMENTS_LIST:
+			auto len = min(64, info->DataLength);
+			for (DWORD i = 0; i < len; i++) {
+				printf("%02X ", p[i]);
+			}
+			printf("\n");
+			break;
+	}
+}
+
+NTSTATUS EnumerateKeyValues(HANDLE hKey) {
+	ULONG len;
+	for (ULONG i = 0; ; i++) {
+		// first call to get size
+		auto status = NtEnumerateValueKey(hKey, i, KeyValueFullInformation, nullptr, 0, &len);
+		if (status == STATUS_NO_MORE_ENTRIES)
+			break;
+
+		auto buffer = std::make_unique<BYTE[]>(len);
+		// make the real call
+		status = NtEnumerateValueKey(hKey, i, KeyValueFullInformation, buffer.get(), len, &len);
+		if (!NT_SUCCESS(status))
+			continue;
+
+		auto info = (KEY_VALUE_FULL_INFORMATION*)buffer.get();
+
+		printf("Name: %ws Type: %s (%u) Data Size: %u bytes Data: ",
+			std::wstring(info->Name, info->NameLength / sizeof(WCHAR)).c_str(),
+			RegistryTypeToString(info->Type), info->Type,
+			info->DataLength);
+
+		DisplayData(info);
+	}
+	return STATUS_SUCCESS;
+}
+
+int wmain(int argc, const wchar_t* argv[]) {
+	if (argc < 2) {
+		printf("Usage: enumkeyvalues <key>\n");
+		return 0;
+	}
+
+	HANDLE hKey;
+	UNICODE_STRING keyName;
+	RtlInitUnicodeString(&keyName, argv[1]);
+	OBJECT_ATTRIBUTES keyAttr;
+	InitializeObjectAttributes(&keyAttr, &keyName, 0, nullptr, nullptr);
+	auto status = NtOpenKey(&hKey, KEY_QUERY_VALUE, &keyAttr);
+	if (!NT_SUCCESS(status)) {
+		printf("Failed to open key (0x%X)\n", status);
+		return status;
+	}
+
+	EnumerateKeyValues(hKey);
+	NtClose(hKey);
+}
+
---
+++ Chapter13/KeyInfo/KeyInfo.cpp
@@ -0,0 +1,82 @@
+#include <phnt_windows.h>
+#include <phnt.h>
+#include <stdio.h>
+#include <memory>
+#include <string>
+#include <format>
+
+#pragma comment(lib, "ntdll")
+
+std::string FormatTime(LARGE_INTEGER& dt) {
+	TIME_FIELDS tf;
+	RtlTimeToTimeFields(&dt, &tf);
+	return std::format("{:04}/{:02}/{:02} {:02}:{:02}:{:02}.{:03}",
+		tf.Year, tf.Month, tf.Day, tf.Hour, tf.Minute, tf.Second, tf.Milliseconds);
+
+}
+
+void DisplayInfo(HANDLE hKey) {
+	BYTE buffer[1024];
+	ULONG len;
+	if (NT_SUCCESS(NtQueryKey(hKey, KeyNameInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_NAME_INFORMATION*)buffer;
+		printf("Full Name: %ws\n", std::wstring(info->Name, info->NameLength / sizeof(WCHAR)).c_str());
+	}
+	if (NT_SUCCESS(NtQueryKey(hKey, KeyBasicInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_BASIC_INFORMATION*)buffer;
+		printf("Name: %ws\n", std::wstring(info->Name, info->NameLength / sizeof(WCHAR)).c_str());
+		printf("Write time: %s\n", FormatTime(info->LastWriteTime).c_str());
+	}
+	if (NT_SUCCESS(NtQueryKey(hKey, KeyFullInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_FULL_INFORMATION*)buffer;
+		if (info->ClassLength) {
+			printf("Class: %ws\n", std::wstring(info->Class, info->ClassLength / sizeof(WCHAR)).c_str());
+		}
+		printf("Subkeys: %u\n", info->SubKeys);
+		printf("Values: %u\n", info->Values);
+		printf("Max class length: %u\n", info->MaxClassLen);
+		printf("Max name length: %u\n", info->MaxNameLen);
+		printf("Max value name length: %u\n", info->MaxValueNameLen);
+		printf("Max data length: %u\n", info->MaxValueDataLen);
+	}
+	else if (NT_SUCCESS(NtQueryKey(hKey, KeyCachedInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_CACHED_INFORMATION*)buffer;
+		printf("Subkeys: %u\n", info->SubKeys);
+		printf("Values: %u\n", info->Values);
+		printf("Max name length: %u\n", info->MaxNameLen);
+		printf("Max value name length: %u\n", info->MaxValueNameLen);
+		printf("Max data length: %u\n", info->MaxValueDataLen);
+	}
+	if (NT_SUCCESS(NtQueryKey(hKey, KeyFlagsInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_FLAGS_INFORMATION*)buffer;
+		printf("Wow64 flags: 0x%X\n", info->Wow64Flags);
+		printf("Key flags: 0x%X\n", info->KeyFlags);	
+		printf("Control flags: 0x%X\n", info->ControlFlags);
+	}
+	if (NT_SUCCESS(NtQueryKey(hKey, KeyTrustInformation, buffer, sizeof(buffer), &len))) {
+		auto info = (KEY_TRUST_INFORMATION*)buffer;
+		printf("Trusted: %s\n", info->TrustedKey ? "Yes" : "No");
+	}
+}
+
+int wmain(int argc, const wchar_t* argv[]) {
+	if (argc < 2) {
+		printf("Usage: enumkeyvalues <key>\n");
+		return 0;
+	}
+
+	HANDLE hKey;
+	UNICODE_STRING keyName;
+	RtlInitUnicodeString(&keyName, argv[1]);
+	OBJECT_ATTRIBUTES keyAttr;
+	InitializeObjectAttributes(&keyAttr, &keyName, 0, nullptr, nullptr);
+	auto status = NtOpenKey(&hKey, KEY_QUERY_VALUE, &keyAttr);
+	if (!NT_SUCCESS(status)) {
+		printf("Failed to open key (0x%X)\n", status);
+		return status;
+	}
+
+	DisplayInfo(hKey);
+	NtClose(hKey);
+}
+
