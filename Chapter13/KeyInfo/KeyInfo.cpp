853dec157c038340572af95d87941c1659c26d94
Pavel Yosifovich <zodiacon@live.com>
2025-05-05 10:27:15 -0400

    minor tweaks

--- Chapter13/KeyInfo/KeyInfo.cpp
+++ Chapter13/KeyInfo/KeyInfo.cpp
@@ -24,34 +24,34 @@ void DisplayInfo(HANDLE hKey) {
 	}
 	if (NT_SUCCESS(NtQueryKey(hKey, KeyBasicInformation, buffer, sizeof(buffer), &len))) {
 		auto info = (KEY_BASIC_INFORMATION*)buffer;
-		printf("Name: %ws\n", std::wstring(info->Name, info->NameLength / sizeof(WCHAR)).c_str());
+		printf("Name: %S\n", std::wstring(info->Name, info->NameLength / sizeof(WCHAR)).c_str());
 		printf("Write time: %s\n", FormatTime(info->LastWriteTime).c_str());
 	}
 	if (NT_SUCCESS(NtQueryKey(hKey, KeyFullInformation, buffer, sizeof(buffer), &len))) {
 		auto info = (KEY_FULL_INFORMATION*)buffer;
 		if (info->ClassLength) {
-			printf("Class: %ws\n", std::wstring(info->Class, info->ClassLength / sizeof(WCHAR)).c_str());
+			printf("Class: %S\n", std::wstring(info->Class, info->ClassLength / sizeof(WCHAR)).c_str());
 		}
-		printf("Subkeys: %u\n", info->SubKeys);
-		printf("Values: %u\n", info->Values);
-		printf("Max class length: %u\n", info->MaxClassLength);
-		printf("Max name length: %u\n", info->MaxNameLength);
-		printf("Max value name length: %u\n", info->MaxValueNameLength);
-		printf("Max data length: %u\n", info->MaxValueDataLength);
+		printf("Subkeys: %lu\n", info->SubKeys);
+		printf("Values: %lu\n", info->Values);
+		printf("Max class length: %lu\n", info->MaxClassLength);
+		printf("Max name length: %lu\n", info->MaxNameLength);
+		printf("Max value name length: %lu\n", info->MaxValueNameLength);
+		printf("Max data length: %lu\n", info->MaxValueDataLength);
 	}
 	else if (NT_SUCCESS(NtQueryKey(hKey, KeyCachedInformation, buffer, sizeof(buffer), &len))) {
 		auto info = (KEY_CACHED_INFORMATION*)buffer;
-		printf("Subkeys: %u\n", info->SubKeys);
-		printf("Values: %u\n", info->Values);
-		printf("Max name length: %u\n", info->MaxNameLength);
-		printf("Max value name length: %u\n", info->MaxValueNameLength);
-		printf("Max data length: %u\n", info->MaxValueDataLength);
+		printf("Subkeys: %lu\n", info->SubKeys);
+		printf("Values: %lu\n", info->Values);
+		printf("Max name length: %lu\n", info->MaxNameLength);
+		printf("Max value name length: %lu\n", info->MaxValueNameLength);
+		printf("Max data length: %lu\n", info->MaxValueDataLength);
 	}
 	if (NT_SUCCESS(NtQueryKey(hKey, KeyFlagsInformation, buffer, sizeof(buffer), &len))) {
 		auto info = (KEY_FLAGS_INFORMATION*)buffer;
-		printf("Wow64 flags: 0x%X\n", info->Wow64Flags);
-		printf("Key flags: 0x%X\n", info->KeyFlags);	
-		printf("Control flags: 0x%X\n", info->ControlFlags);
+		printf("Wow64 flags: 0x%lX\n", info->Wow64Flags);
+		printf("Key flags: 0x%lX\n", info->KeyFlags);
+		printf("Control flags: 0x%lX\n", info->ControlFlags);
 	}
 	if (NT_SUCCESS(NtQueryKey(hKey, KeyTrustInformation, buffer, sizeof(buffer), &len))) {
 		auto info = (KEY_TRUST_INFORMATION*)buffer;
@@ -61,7 +61,7 @@ void DisplayInfo(HANDLE hKey) {
 
 int wmain(int argc, const wchar_t* argv[]) {
 	if (argc < 2) {
-		printf("Usage: enumkeyvalues <key>\n");
+		printf("Usage: keyinfo <key>\n");
 		return 0;
 	}
 
@@ -72,7 +72,7 @@ int wmain(int argc, const wchar_t* argv[]) {
 	InitializeObjectAttributes(&keyAttr, &keyName, 0, nullptr, nullptr);
 	auto status = NtOpenKey(&hKey, KEY_QUERY_VALUE, &keyAttr);
 	if (!NT_SUCCESS(status)) {
-		printf("Failed to open key (0x%X)\n", status);
+		printf("Failed to open key (0x%lX)\n", status);
 		return status;
 	}
 
